6a06ce4437e6a068e1dea32948a896296b75c960
Hardwaregore <hardwaregore101@gmail.com>
2022-07-15 20:36:27 -0500

    Free commits

--- autocommit.sh
+++ autocommit.sh
@@ -4,14 +4,14 @@ while true; do
 	touch a
 	git add .
 	git commit -m "Free commits"
-	git push
+	git push >> /dev/null
 	echo a | tee a
 	git add .
         git commit -m "Free commits"
-        git push   
+        git push >> /dev/null
 	rm -rf a
 	git add .
         git commit -m "Free commits"
-        git push
+        git push >> /dev/null
 done
 
